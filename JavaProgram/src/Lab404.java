8fd1f2296b2695dde04b49d29eadb57c4d16db0d
GBcatW <2213110113@tnistudent.ac.th>
2022-12-01 16:02:52 +0700

    Exercise[12-1-2022]

--- JavaProgram/src/Lab404.java
+++ JavaProgram/src/Lab404.java
@@ -7,7 +7,19 @@ public class Lab404 {
 		System.out.print("Text : ");
 		String text = scan.nextLine().toLowerCase();
 		text = text.replace(" ","");
-		
+		int x = text.length();
+		int y = x/2-1;
+		boolean isPalindrome = true;
+		for(int i = 0; i<=y;i++) {
+			if(text.charAt(i) == text.charAt(text.length()-1-i));
+			else
+			{
+				isPalindrome = false;
+				break;
+			}
+		}
+		String result = isPalindrome? "It is palindrome":"It is not palindrome";
+		System.out.print(result);
 	}
 
 }
